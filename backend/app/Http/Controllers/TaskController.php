0abd1b2d472b0a6d46a2da69c328d2dc6708b518
Maurício Cardoso <mauricioerick17@gmail.com>
2023-05-07 23:13:47 -0400

    feat: Adding api structure

---
+++ backend/app/Http/Controllers/TaskController.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Helpers\Logger;
+use App\Http\Requests\TaskRequest;
+use App\Models\Task;
+use Exception;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
+
+class TaskController extends Controller
+{
+    public function index()
+    {
+        try{
+            $tasks = Task::all();
+            return response()->json($tasks);
+
+        } catch (Exception $e) {
+            $error = 'Não foi possível carregar a lista de Tarefas';
+            Logger::log($e, $error);
+            return response()->json(['errors' => $error], 500);
+
+        }
+    }
+
+    public function store(TaskRequest $request)
+    {
+        DB::beginTransaction();
+        try{
+            $task = Task::create($request->all());
+            DB::commit();
+            return response()->json(['task' => $task], 201);
+
+        } catch (Exception $e) {
+            DB::rollBack();
+            $error = 'Não foi possível criar uma nova Tarefa';
+            Logger::log($e, $error);
+            return response()->json(['errors' => $error], 500);
+
+        }
+    }
+
+    public function update(TaskRequest $request, Task $task)
+    {
+        DB::beginTransaction();
+        try{
+            $task->update($request->all());
+            DB::commit();
+            return response()->json(null, 204);
+
+        } catch (Exception $e) {
+            DB::rollBack();
+            $error = 'Não foi possível atualizar a Tarefa selecionada';
+            Logger::log($e, $error);
+            return response()->json(['errors' => $error], 500);
+
+        }
+
+    }
+
+    public function destroy(Task $task)
+    {
+        DB::beginTransaction();
+        try{
+            $task->delete();
+            DB::commit();
+            return response()->json(null, 204);
+
+        } catch (Exception $e) {
+            DB::rollBack();
+            $error = 'Não foi possível excluir a Tarefa selecionada';
+            Logger::log($e, $error);
+            return response()->json(['errors' => $error], 500);
+
+        }
+
+    }
+}
---
+++ backend/app/Http/Requests/TaskRequest.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class TaskRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'name' => ['required', 'string', 'min:3'],
+            'status' => ['required', 'boolean'],
+        ];
+    }
+
+    public function messages(): array
+    {
+        return [
+            'name.required' => 'O campo nome é obrigatório',
+            'name.string' => 'O campo nome deve ser um conjunto de caracteres',
+            'name.min' => 'O campo :attribute deve ter no mínimo :min caracteres',
+            'status.required' => 'O campo status é obrigatório',
+            'status.boolean' => 'O campo status deve ser verdadeiro ou falso'
+        ];
+    }
+
+
+}
